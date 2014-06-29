7a1c04b8a2b4d5299caaef2302c404bbcbdf6edc
Pbs <pbsphp@yandex.ru>
2014-06-29 15:39:55 +0400

    install and uninstall commands for make

---
+++ install.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+DEVICE_NAME="nikitich"
+
+
+# Load module
+
+echo "Loading module..."
+
+insmod nikitich.ko || {
+    echo "Fail!"
+    exit 1
+}
+
+
+# Get device major number
+
+echo "Getting major number..."
+
+major=`sed -n "s;\([0-9]\+\) $DEVICE_NAME;\1;p" /proc/devices`
+
+if [ -z "$major" ]; then
+    echo "Cannot find _ in /proc/devices. Fail!"
+    exit 1
+fi
+
+
+echo "Creating /dev/$DEVICE_NAME..."
+
+# Create /dev/DEVICE
+
+mknod /dev/$DEVICE_NAME c $major 0 || {
+    echo "Cannot create /dev/$DEVICE_NAME. Fail!"
+    exit 1
+}
+
+
+echo "Done!"
